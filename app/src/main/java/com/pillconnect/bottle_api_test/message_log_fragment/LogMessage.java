714110a9429cb20e6bd4558c4f0014ebc97cf694
Adam Howard <adam.howard@elucid-mhealth.com>
2018-01-23 18:33:30 +0000

    refactor: move message log fragment classes into their own package

---
+++ app/src/main/java/com/pillconnect/bottle_api_test/message_log_fragment/LogMessage.java
@@ -0,0 +1,20 @@
+package com.pillconnect.bottle_api_test.message_log_fragment;
+
+/**
+ * @author Adam Howard
+ * @date 23/01/18
+ */
+final class LogMessage {
+    public final String message;
+    public final long time;
+
+    public LogMessage(String msg) {
+        this.message = msg;
+        this.time = System.currentTimeMillis();
+    }
+
+    @Override
+    public String toString() {
+        return "message: \"" + message + "\" at " + time;
+    }
+}
---
+++ app/src/main/java/com/pillconnect/bottle_api_test/message_log_fragment/MessageAdapter.java
@@ -0,0 +1,64 @@
+package com.pillconnect.bottle_api_test.message_log_fragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.pillconnect.bottle_api_test.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Adam Howard
+ * @date 23/01/18
+ */
+class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+    private List<LogMessage> msgList = null;
+
+    public MessageAdapter(List<LogMessage> msgList) {
+        this.msgList = msgList;
+    }
+
+    @Override
+    public void onBindViewHolder(MessageViewHolder holder, int position) {
+        String msg = msgList.get(position).message;
+        if (msg == null) {
+            msg = "<null>";
+        }
+        // Show message in linearlayout.
+        holder.leftMsgTextView.setText(msg);
+    }
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.message_item_view, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (msgList == null) {
+            // if this is null, why wasn't it initialised in the constructor?
+            msgList = new ArrayList<LogMessage>();//fixme: questionable.
+        }
+        return msgList.size();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        //LinearLayout leftMsgLayout;
+        TextView leftMsgTextView;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+
+            if (itemView != null) {
+                //leftMsgLayout = (LinearLayout) itemView.findViewById(R.id.message_layout);
+                leftMsgTextView = (TextView) itemView.findViewById(R.id.message_text_view);
+            }
+        }
+    }
+}
